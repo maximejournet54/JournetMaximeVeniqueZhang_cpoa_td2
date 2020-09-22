f1e93e2d0c5ef4005d54d1dcf19e45659b981341
Zhang269u <zhang269u@etu.univ-lorraine.fr>
2020-09-22 10:34:57 +0200

    mise a jour MYSQLClientDAO+Client

--- src/Application/Client.java
+++ src/Application/Client.java
@@ -1,25 +1,42 @@
 package Application;
-
 import java.sql.*;
 
 public class Client {
-    public static void add(int id_client, String nom, String prenom, String mdp, int num, String voie, int cp, String ville, String pays){
+    String id_client,nom,prenom,identifiant,mdp,num,voie,cp,ville,pays;
+
+    public Client(String id_client, String nom, String prenom, String identifiant, String mdp, String num, String voie, String cp, String ville, String pays) {
+        this.id_client = id_client;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.identifiant = identifiant;
+        this.mdp = mdp;
+        this.num = num;
+        this.voie = voie;
+        this.cp = cp;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    public static void create(Object T){
         try {
+            Client c = (Client) T;
             Connection laConnexion = Connexion.creeConnexion();
             Statement requete= laConnexion.createStatement();
-            String query="INSERT INTO Client VALUES("+id_client+",'"+nom+"','"+prenom+"','"+mdp+"','"+num+"','"+voie+"','"+cp+"','"+ville+"','"+pays+"')";
+            String query="INSERT INTO Client VALUES("+c.id_client+","+c.nom+","+c.prenom+","+c.mdp+","+c.num+","+c.voie+","+c.cp+","+c.ville+","+c.pays+")";
             requete.executeUpdate(query);
-            System.out.println("Produit ajoute");
+            System.out.println("Client ajoute");
+            System.out.println("Client ajoute");
         } catch(SQLException sqle){
             System.out.println("Probleme select:" +sqle.getMessage());
         }            
     }
 
-    public static void delete(int id_client){
+    public static void delete(Object T){
         try {
+            Client c = (Client) T;
             Connection laConnexion = Connexion.creeConnexion();
             Statement requete= laConnexion.createStatement();
-            String query="delete from Client where id_client="+id_client;
+            String query="delete from Client where id_client="+c.id_client;
             requete.executeUpdate(query);
             System.out.println("client supprime");
             } catch(SQLException sqle){
@@ -27,11 +44,12 @@ public class Client {
         }          
     }
 
-    public static void update(int id_client){
+    public static void update(Object T){
         try {
+            Client c = (Client) T;
             Connection laConnexion = Connexion.creeConnexion();
             Statement requete= laConnexion.createStatement();
-            String query="update from Client where id_client="+id_client;
+            String query="update from Client where id_client="+c.id_client;
             requete.executeUpdate(query);
             System.out.println("client mis a jour");
             } catch(SQLException sqle){
--- src/Application/MYSQLClientDAO.java
+++ src/Application/MYSQLClientDAO.java
@@ -1,54 +1,77 @@
 package Application;
+import java.beans.Expression;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import java.sql.*;
+public class MYSQLClientDAO implements DAO<Client>{
 
-public class MYSQLClientDAO {
-    public String getNom(int id_client){
-        //mettre requete sql
-        String nom=null;
+    @Override
+    public boolean create(Object T) {
         try {
-            Connection laConnexion = Connexion.creeConnexion();
-            Statement requete= laConnexion.createStatement();
-            String query="select nom from client where id_client="+id_client;
-            ResultSet res=requete.executeQuery(query);
-            visuel= res.getString("nom");
-        } catch(SQLException sqle){
-            System.out.println("Probleme select:" +sqle.getMessage());
+            Client.create(T);
+            return true;
+
+        }catch (Exception e){
+            return false;
         }
-        return nom;
-    }
-    public String getPrenom(int id_client){
-        //mettre requete sql
-        String prenom=null;
-        try {
-            Connection laConnexion = Connexion.creeConnexion();
-            Statement requete= laConnexion.createStatement();
-            String query="select prenom from client where id_client="+id_client;
-            ResultSet res=requete.executeQuery(query);
-            visuel= res.getString("prenom");
-        } catch(SQLException sqle){
-            System.out.println("Probleme select:" +sqle.getMessage());
-        }
-        return prenom;
     }
 
+    @Override
+    public boolean delete(Object T) {
+        try {
+            Client.delete(T);
+            return true;
 
-    public String getAdress(int id_client){
-        //mettre requete sql
-        String adresse=null;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Object T) {
+        try {
+            Client.update(T);
+            return true;
+
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public Client getById(int id_client) {
         try {
             Connection laConnexion = Connexion.creeConnexion();
-            Statement requete= laConnexion.createStatement();
-            String query="select num,voie,cp,ville,pays from client where id_client="+id_client;
-            ResultSet res=requete.executeQuery(query);
-            adresse= res.getString("num")+res.getString("voie")+
-            res.getString("cp")+
-            res.getString("ville")+res.getString("pays");
-        } catch(SQLException sqle){
-            System.out.println("Probleme select:" +sqle.getMessage());
+            Statement requete = laConnexion.createStatement();
+            ResultSet res = requete.executeQuery("select * from Client where id_client ="+id_client);
+            while (res.next()) {
+                String id =res.getString("id_client");
+                String nom=res.getString("nom");
+                String prenom=res.getString("prenom");
+                String identifiant=res.getString("identifiant");
+                String mdp=res.getString("mot_de_passe");
+                String num=res.getString("adr_numero");
+                String voie=res.getString("adr_voie");
+                String cp=res.getString("adr_code_postal");
+                String ville=res.getString("adr_ville");
+                String pays=res.getString("adr_pays");
+                return new Client(id,nom,prenom,identifiant,mdp,num,voie,cp,ville,pays);
+
+            }
+
+
+            if (res != null)
+                res.close();
+            if (requete != null)
+                requete.close();
+            if (laConnexion != null)
+                laConnexion.close();
+
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select " + sqle.getMessage());
         }
-        return adresse;
+        return null;
     }
 }
-    
-
